d67dfad9d1ab310ab58794f5aeedb8ea12eb65b5
Remo <81964236+remoremote@users.noreply.github.com>
2023-05-16 21:00:32 +0200

    start api call

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import './Homepage.css'
 
 function Homepage() {
+  //set up useEffect
+  useEffect(
+    ()=> {
+       axios.get(``)
+       .then(res)
+       .catch(err => console.log(err)) //make api call
+    }, []
+  )
+
   return (
     <div>
       <button>All</button>
