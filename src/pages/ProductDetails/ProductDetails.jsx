8581d6e416345e0081510ab382c7aac2fd9de8ce
Remo <81964236+remoremote@users.noreply.github.com>
2023-06-04 14:37:03 +0200

    product detail page

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './ProductDetails.css'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 function ProductDetails() {
+  //show data for a specific product
+  //the id is in the url
+  //use hook to retrieve the value
+  const {productId} = useParams()
+
+  //create state to hold product data
+  const [product, setProduct] = React.useState('')
+
+
+  //https://fakestoreapi.com/products/1
+  //I need to get the data when the page loads
+  React.useEffect(
+    ()=>{
+      console.log('details')
+      //make api call to get data
+      axios.get(`https://fakestoreapi.com/products/${productId}`)
+      .then(res =>{
+        console.log(res.data)
+        //store data in state
+        setProduct(res.data)
+      })
+      .catch(err => console.log(err))
+
+    }, [] //this means it only loads once
+  )
+  
   return (
-    <div>ProductDetails</div>
+    <div className='details-background'>
+      <div className="details-container">
+        <img src={product.image} alt="" />
+        <div className="details-info">
+          <p>{product.title}</p>
+          <p>${product.price}</p>
+          <p>Description</p>
+          <p className='details-desc'>{product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
